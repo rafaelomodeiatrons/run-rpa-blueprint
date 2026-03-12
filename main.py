9f572bf52ca2e811802d6a56fb51117ed3583eeb
rafaelomodei <rafael.omodei@outlook.com>
2026-03-12 09:21:37 -0300

    refator: refactored json

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ ENDPOINT = "http://localhost:3000/automations/blueprint-runner/activate"
 TENANT_ID = "0192d800-65a7-7f49-83aa-80739accb137"
 
 # Ajuste aqui (segundos)
-SLEEP_MINUTES = 40
+SLEEP_MINUTES = 0
 
 # Arquivo com os 671 itens (o que você gerou)
-JSON_FILE_PATH = "cpfl_542.json"
+JSON_FILE_PATH = "blueprints_belo_horizonte_mg_indice_2026-02-17.json"
 
 
 def load_items(path: str) -> List[Dict[str, Any]]:
