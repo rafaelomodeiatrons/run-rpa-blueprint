198e868fe169ad12bed32867af43570f98230ae6
rafaelomodei <rafael.omodei@outlook.com>
2026-03-16 21:23:57 -0300

    feat: updated

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TENANT_ID = "0192d800-65a7-7f49-83aa-80739accb137"
 SLEEP_MINUTES = 0
 
 # Arquivo com os 671 itens (o que você gerou)
-JSON_FILE_PATH = "cnd_bh_57.json"
+JSON_FILE_PATH = "blueprints_ENERGIA_cemig_blueprints_com_todos_os_indice_252.json"
 
 
 def load_items(path: str) -> List[Dict[str, Any]]:
